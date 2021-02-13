02c3affd473e2468618a8923fe72eaf13e260c54
garu12 <garu12@hotmail.com>
2021-02-12 20:08:22 -0500

    Entidad: Cliente, Class Cliente para JPA

---
+++ src/main/java/com/spring/datajpa/app/models/entity/Cliente.java
@@ -0,0 +1,67 @@
+package com.spring.datajpa.app.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+/**  Getters and Setters  ****************************************************************************************************************/
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    private static final long SerialVersionUID = 1L;
+}
