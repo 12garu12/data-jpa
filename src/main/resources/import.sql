6326cf04603ef3b55d2247695db3d6e92563de4c
garu12 <garu12@hotmail.com>
2021-02-12 23:13:40 -0500

    Creando la vista listar.htm para mostrar los datos de la base de datos y añadiendo bootstrap para el lado del front-end

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientes(id, first_name, last_name, email, create_at) VALUES (1, 'Andrés', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-29');
+INSERT INTO clientes(id, first_name, last_name, email, create_at) VALUES (2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-29');
+
